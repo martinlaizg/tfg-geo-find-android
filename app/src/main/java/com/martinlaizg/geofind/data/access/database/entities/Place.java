dafc9dd3bc4cd0f83c1828ced016be7a8db68588
Martin Laiz <martinlaizg@gmail.com>
2019-06-06 16:32:09 +0200

    Añadida pregunta a Place entity

--- app/src/main/java/com/martinlaizg/geofind/data/access/database/entities/Place.java
+++ app/src/main/java/com/martinlaizg/geofind/data/access/database/entities/Place.java
@@ -32,6 +32,12 @@ public class Place {
 	private Date updated_at;
 	private Date updated;
 
+	// Question
+	private String question;
+	private String answer;
+	private String answer2;
+	private String answer3;
+
 	public Place(Integer id, String name, Double lat, Double lon, Integer tour_id,
 			String description, Integer order, Date created_at, Date updated_at, Date updated) {
 		this.id = id;
@@ -149,4 +155,35 @@ public class Place {
 		return DateUtils.isDateExpire(updated);
 	}
 
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public String getAnswer2() {
+		return answer2;
+	}
+
+	public void setAnswer2(String answer2) {
+		this.answer2 = answer2;
+	}
+
+	public String getAnswer3() {
+		return answer3;
+	}
+
+	public void setAnswer3(String answer3) {
+		this.answer3 = answer3;
+	}
 }
